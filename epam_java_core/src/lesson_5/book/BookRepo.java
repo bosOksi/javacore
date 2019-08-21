fcba29d9a2236e406aed28f87b5dc4a6afe1b051
bosOksi <49459472+bosOksi@users.noreply.github.com>
2019-08-21 10:17:23 +0300

    Delete BookRepo.java

--- epam_java_core/src/lesson_5/book/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package lesson_5.book;
-
-public interface BookRepo {
-    void saveBook(Book book);
-    void searchBook(Book book);
-    int getCountOfBooks();
-    void getBooksSortedByName();
-    void removeBook(Book book);
-}
