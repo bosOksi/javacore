df7c7983552ec4eb59cb5dbf14fb1bb56ba954a5
bosOksi <49459472+bosOksi@users.noreply.github.com>
2019-08-21 10:17:55 +0300

    Delete AuthorRepo.java

--- epam_java_core/src/lesson_5/author/AuthorRepo.java
+++
@@ -1,9 +0,0 @@
-package lesson_5.author;
-
-public interface AuthorRepo {
-    void saveAuthor(Author author);
-    void searchAuthor(Author author);
-    int getCountOfAuthor();
-    void getAuthorSortedByLastName();
-    void removeAuthor(Author author);
-}
