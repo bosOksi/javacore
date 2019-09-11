931c53613866398f78a7f549baefd425daa88617
bosOksi <49459472+bosOksi@users.noreply.github.com>
2019-09-11 14:37:54 +0300

    Add files via upload

--- epam_java_core/src/lesson_5/LibraryDemo.java
+++ epam_java_core/src/lesson_5/LibraryDemo.java
@@ -16,33 +16,133 @@ import lesson_5.book.repo.BookRepoArrayImpl;
 import lesson_5.book.repo.BookRepoCollectionImpl;
 import lesson_5.book.service.BookService;
 import lesson_5.book.service.BookServiceImpl;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static lesson_5.common.utils.CollectionUtils.mutableCollectionOf;
 
 public class LibraryDemo {
 
     public static void main(String[] args) {
-        initData(InitType.FROM_MEMORY);
+        //initData(InitType.FROM_MEMORY);
+        initData(InitType.FROM_FILE);
     }
+
     public static void initData(InitType initType) {
         switch (initType) {
             case FROM_FILE:
-                //////////////////////////////////////////
-                //////////////////////////////////////////
+                try {
+                    List<String> strings = readFile("C:\\Users\\Oksana\\Desktop\\Library.txt");
+                    int isBook = -1;
+                    for (String line : strings) {
+                        if (line.contains("Entity: Book")) {
+                            isBook = 1;
+                        } else if (line.contains("Entity: Author")) {
+                            isBook = 0;
+                        }
+                        String bookName = null;
+                        String publishYear = null;
+                        String authorLastName = null;
+                        String authorName = null;
+                        String authorYearOfBorn = null;
+                        if (isBook == 1) {
+                            if (line.contains("name:")) {
+                                String[] lineArray;
+                                lineArray = line.split("\"");
+                                bookName = lineArray[1];
+                            }
+                            if (line.contains("publishYear:")) {
+                                String[] lineArray;
+                                lineArray = line.split(":");
+                                publishYear = lineArray[1];
+                            }
+
+                            InputBook inputBookFromFile = new InputBook();
+                            if (publishYear != null && bookName != null) {
+                                inputBookFromFile.setName(bookName);
+                                inputBookFromFile.setPublishYear(Integer.parseInt(publishYear.trim()));
+                            }
+                            if (line.contains("HandWrittenBook")) {
+                                Book bookFromFile = valueOfForHandWrittenBook(inputBookFromFile);
+                            } else if (line.contains("PrintedBook")) {
+                                Book bookFromFile = valueOfForPrintedBook(inputBookFromFile);
+                            }
+                        } else if (isBook == 0) {
+                            if (line.contains("lastName:")) {
+                                String[] lineArray;
+                                lineArray = line.split("\"");
+                                authorLastName = lineArray[1];
+                            }
+                            if (line.contains("name:")) {
+                                String[] lineArray;
+                                lineArray = line.split(":");
+                                authorName = lineArray[1];
+                            }
+                            if (line.contains("yearOfBorn:")) {
+                                String[] lineArray;
+                                lineArray = line.split(":");
+                                authorYearOfBorn = lineArray[1];
+                            }
+                            InputAuthor inputAuthorFromFile = new InputAuthor();
+                            if (authorName != null && authorLastName != null && authorYearOfBorn != null) {
+                                inputAuthorFromFile.setLastName(authorLastName);
+                                inputAuthorFromFile.setName(authorName);
+                                inputAuthorFromFile.setYearOfBorn(Integer.parseInt(authorYearOfBorn.trim()));
+                            }
+
+                            /*Author author = valueOfForHandWrittenBook(inputAuthor);
+
+                            author.setBooks(mutableCollectionOf(book1, book2));
+                            book1.setAuthors(mutableCollectionOf(author, author));
+                            book2.setAuthors(mutableCollectionOf(author));
+
+                            bookService.add(book1);
+                            bookService.add(book2);
+
+                            authorService.add(author);
+                            */
+                        }
+                    }
+                    String storageType = "collection";
+                    BookRepo bookRepo = null;
+                    AuthorRepo authorRepo = null;
+
+                    if (storageType.equals("arrays")) {
+                        bookRepo = new BookRepoArrayImpl();
+                        authorRepo = new AuthorRepoArrayImpl();
+                    } else if (storageType.equals("collection")) {
+                        bookRepo = new BookRepoCollectionImpl();
+                        authorRepo = new AuthorRepoCollectionImpl();
+                    }
+
+                    AuthorService authorService = new AuthorServiceImpl(authorRepo, bookRepo);
+                    BookService bookService = new BookServiceImpl(bookRepo);
+                    initData(bookService, authorService);
+
+                    bookService.print();
+                    authorService.print();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             case FROM_MEMORY:
                 String storageType = "collection";
                 BookRepo bookRepo = null;
                 AuthorRepo authorRepo = null;
 
-                if (storageType.equals("arrays")){
+                if (storageType.equals("arrays")) {
                     bookRepo = new BookRepoArrayImpl();
                     authorRepo = new AuthorRepoArrayImpl();
-                }else if (storageType.equals("collection")){
+                } else if (storageType.equals("collection")) {
                     bookRepo = new BookRepoCollectionImpl();
                     authorRepo = new AuthorRepoCollectionImpl();
                 }
 
-                AuthorService authorService = new AuthorServiceImpl(authorRepo,bookRepo);
+                AuthorService authorService = new AuthorServiceImpl(authorRepo, bookRepo);
                 BookService bookService = new BookServiceImpl(bookRepo);
                 initData(bookService, authorService);
 
@@ -51,6 +151,18 @@ public class LibraryDemo {
                 break;
         }
     }
+
+    private static List<String> readFile(String file) throws IOException {
+        List<String> content = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                content.add(line);
+            }
+        }
+        return content;
+    }
+
     private static void initData(BookService bookService, AuthorService authorService) {
         InputBook inputBook1 = new InputBook();
         inputBook1.setName("Evgeny Onegin");
@@ -68,8 +180,8 @@ public class LibraryDemo {
         inputAuthor.setYearOfBorn(1100);
         Author author = valueOfForHandWrittenBook(inputAuthor);
 
-        author.setBooks(mutableCollectionOf(book1,book2));
-        book1.setAuthors(mutableCollectionOf(author,author));
+        author.setBooks(mutableCollectionOf(book1, book2));
+        book1.setAuthors(mutableCollectionOf(author, author));
         book2.setAuthors(mutableCollectionOf(author));
 
         bookService.add(book1);
